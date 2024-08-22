41bb15a34fa7bed351cb9b046d32eed28ae343e6
AbiHerobrineYT <himagara.abimanyu@gmail.com>
2024-08-22 16:41:53 +0700

    final

--- SecondWindow.py
+++ SecondWindow.py
@@ -1,7 +1,15 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from instr import *
 from ThirdWindow import *
+
+class Experiment():
+    def __init__(self, age, pulse, final1, final2):
+        self.age = age
+        self.t1 = pulse
+        self.t2 = final1
+        self.t3 = final2
 
 class TestWin(QWidget):
     def __init__(self):
@@ -26,12 +34,16 @@ class TestWin(QWidget):
         self.pulsetest = QLabel(txt_test1)
         self.squat = QLabel(txt_test2)
         self.finaltest = QLabel(txt_test3)
-        self.entername = QLineEdit(txt_hintname)
-        self.enterage = QLineEdit(txt_hintage)
-        self.enterpulse = QLineEdit(txt_hinttest1)
-        self.entersquat = QLineEdit(txt_hinttest2)
-        self.enterfinal = QLineEdit(txt_hinttest3)
-        self.enterfinal2 = QLineEdit(txt_hinttest3)
+        self.entername = QLineEdit()
+        self.entername.setPlaceholderText(txt_hintname)
+        self.enterage = QLineEdit()
+        self.enterage.setPlaceholderText(txt_hintage)
+        self.enterpulse = QLineEdit()
+        self.enterpulse.setPlaceholderText(txt_hinttest1)
+        self.enterfinal = QLineEdit()
+        self.enterfinal.setPlaceholderText(txt_hinttest3)
+        self.enterfinal2 = QLineEdit()
+        self.enterfinal2.setPlaceholderText(txt_hinttest3)
         self.startpulse = QPushButton(txt_starttest1)
         self.startsquat = QPushButton(txt_starttest2)
         self.startfinal = QPushButton(txt_starttest3)
@@ -43,31 +55,92 @@ class TestWin(QWidget):
         self.entername.setFixedWidth(100)
         self.enterage.setFixedWidth(100)
         self.enterpulse.setFixedWidth(100)
-        self.entersquat.setFixedWidth(100)
         self.enterfinal.setFixedWidth(100)
         self.enterfinal2.setFixedWidth(100)
-        self.timer = QLabel(txt_timer)
+        self.txt_timer = QLabel(txt_timer)
         self.l_line.addWidget(self.name)
         self.l_line.addWidget(self.entername)
         self.l_line.addWidget(self.age)
         self.l_line.addWidget(self.enterage)
         self.l_line.addWidget(self.pulsetest)
+        self.l_line.addWidget(self.startpulse)
         self.l_line.addWidget(self.enterpulse)
         self.l_line.addWidget(self.squat)
-        self.l_line.addWidget(self.entersquat)
+        self.l_line.addWidget(self.startsquat)
         self.l_line.addWidget(self.finaltest)
+        self.l_line.addWidget(self.startfinal)
         self.l_line.addWidget(self.enterfinal)
         self.l_line.addWidget(self.enterfinal2)
         self.l_line.addWidget(self.send, alignment = Qt.AlignCenter)
-        self.r_line.addWidget(self.timer)
+        self.r_line.addWidget(self.txt_timer)
         self.h_line.addLayout(self.l_line)
         self.h_line.addLayout(self.r_line)
         self.setLayout(self.h_line)
 
+    def timerpulse(self):
+        global time
+        time = time.addSecs(-1)
+        self.txt_timer.setText(time.toString("hh:mm:ss"))
+        self.txt_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.txt_timer.setStyleSheet("color: rgb(0, 0, 0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timertest.stop()
+
+    def timersquat(self):
+        global time
+        time = time.addSecs(-1)
+        self.txt_timer.setText(time.toString("hh:mm:ss")[6:8])
+        self.txt_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.txt_timer.setStyleSheet("color: rgb(0, 0, 0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timertest2.stop()
+
+    def timerfinal(self):
+        global time
+        time = time.addSecs(-1)
+        self.txt_timer.setText(time.toString("hh:mm:ss"))
+        self.txt_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.txt_timer.setStyleSheet("color: rgb(0, 0, 0)")
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45 or int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.txt_timer.setStyleSheet("color: rgb(0, 255, 0)")
+        else:
+            self.txt_timer.setStyleSheet("color: rgb(0, 0, 0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timertest3.stop()
+
+    def timer_test(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timertest = QTimer()
+        self.timertest.timeout.connect(self.timerpulse)
+        self.timertest.start(1000)
+
+    def timer_test2(self):
+        global time
+        time = QTime(0, 0, 31)
+        self.timertest = QTimer()
+        self.timertest.timeout.connect(self.timersquat)
+        self.timertest.start(1500)
+
+    def timer_test3(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timertest = QTimer()
+        self.timertest.timeout.connect(self.timerfinal)
+        self.timertest.start(1000)
 
     def connect(self):
         self.send.clicked.connect(self.next_click)
+        self.startpulse.clicked.connect(self.timer_test)
+        self.startsquat.clicked.connect(self.timer_test2)
+        self.startfinal.clicked.connect(self.timer_test3)
     
     def next_click(self):
         self.hide()
-        self.tw = FinalWin()
+        self.exp = Experiment(
+            int(self.enterage.text()),
+            int(self.enterpulse.text()),
+            int(self.enterfinal.text()),
+            int(self.enterfinal2.text())
+        )
+        self.tw = FinalWin(self.exp)
